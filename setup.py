27e3a24e16687b77f8e83f147b772b88e47783cc
Christopher B Flesche <chris@fletch22.com>
2016-11-28 13:04:59 -0600

    Adding setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dj-dna-streaming-python',
+      version='0.9.1',
+      description='Dow Jones DNA Streaming Project',
+      author='Chris Flesche',
+      author_email='chris.flesche@dowjones.com',
+      url='https://github.dowjones.net/syndicationhub/dj-dna-streaming-python/',
+      packages=['distutils', 'distutils.command'],
+     )
