83b573e3bb6834a0c32a39c62e8b8354ab8740a4
Christopher B Flesche <chris@fletch22.com>
2016-11-28 13:12:00 -0600

    Updating setup again.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='dj-dna-streaming-python',
-      version='0.9.1',
-      description='Dow Jones DNA Streaming Project',
-      author='Chris Flesche',
-      author_email='chris.flesche@dowjones.com',
-      url='https://github.dowjones.net/syndicationhub/dj-dna-streaming-python/',
-      packages=['distutils', 'distutils.command'],
-     )
+setup(
+    name='dj-dna-streaming-python',
+    version='0.9.1',
+    description='Dow Jones DNA Streaming Project',
+    author='Chris Flesche',
+    author_email='chris.flesche@dowjones.com',
+    url='https://github.dowjones.net/syndicationhub/dj-dna-streaming-python/',
+    packages=find_packages(),
+
+    package_data={
+      # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst']
+    },
+
+    # metadata for upload to PyPI
+    license="PSF",
+
+    # could also include long_description, download_url, classifiers, etc.
+    )
