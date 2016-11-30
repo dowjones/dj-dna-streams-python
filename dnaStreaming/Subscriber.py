88f2d9520165114f33de67e9bfa1188c55316ee3
Christopher B Flesche <chris@fletch22.com>
2016-11-30 14:59:04 -0600

    Removing integration tests; only way to make integration tests work is with an active gCloud login (not allowed) or with a present google cloud authentication security file (now allowed). Will do integration testing in client runner.

--- dnaStreaming/Subscriber.py
+++ dnaStreaming/Subscriber.py
@@ -9,35 +9,35 @@ class Subscriber():
         self.stop_subscription = False
 
         config = Config.Config()
-        self.gCloudProjectName = config.get_google_cloud_project_name()
+        self.g_cloud_project_name = config.get_google_cloud_project_name()
 
-        self.userKey = config.get_user_key()
+        self.user_key = config.get_user_key()
         self.topic = config.get_topic()
 
     def subscription(self, pubsub_client, topic_name):
         topic = pubsub_client.topic(topic_name)
-        name = topic_name + "_Live_" + self.user_key()
+        name = topic_name + "_Live_" + self.user_key
         return topic.subscription(name)
 
     def halt_subscription_messages(self):
         self.stop_subscription = True
 
     def get_client(self):
-        return pubsub.Client(project=self.gCloudProjectName)
+        return pubsub.Client(project=self.g_cloud_project_name)
 
     DEFAULT_UNLIMITED_MESSAGES = -1
     def subscribe(self, on_message_callback, topic_name=None, maximum_messages=DEFAULT_UNLIMITED_MESSAGES):
         if topic_name is None:
             topic_name = self.topic
 
-        limitPullCalls = not (maximum_messages == self.DEFAULT_UNLIMITED_MESSAGES)
+        limit_pull_calls = not (maximum_messages == self.DEFAULT_UNLIMITED_MESSAGES)
         pubsub_client = self.get_client()
 
         subscription = self.subscription(pubsub_client, topic_name)
 
         while self.stop_subscription != True:
 
-            if limitPullCalls:
+            if limit_pull_calls:
                 if (maximum_messages <= 0):
                     break
 
@@ -47,6 +47,6 @@ class Subscriber():
                 subscription.acknowledge([ack_id for ack_id, message in results])
                 on_message_callback(message, topic_name)
 
-                if limitPullCalls:
+                if limit_pull_calls:
                     maximum_messages -= 1
 
