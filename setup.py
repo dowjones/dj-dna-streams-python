f2f280d176cb20568b6cbce1c96733cb499a1311
Christopher B Flesche <chris@fletch22.com>
2017-06-29 08:59:12 -0500

    Updating readme because wrong; simplifying requirements to help simplify installation — test directions have their own requirements.txt; updating test requirements.txt because out of date.

--- setup.py
+++ setup.py
@@ -17,16 +17,10 @@ setup(
     include_package_data=True,
 
     install_requires=[
-        'flake8==3.3.0',
         'google-api-python-client==1.6.2',
         'google-cloud-core==0.21.0',
         'google-cloud-pubsub==0.21.0',
         'mock==2.0.0',
-        'pyflakes==1.5.0',
-        'pytest==2.9.2',
-        'pytest-cov==2.3.0',
-        'pytest-mock==1.1',
         'requests==2.13.0'
     ]
-    # could also include long_description, download_url, classifiers, etc.
 )
