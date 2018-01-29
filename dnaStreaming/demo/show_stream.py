c169853bbd94ce8170dd6911a9288002d5995fff
Christopher B Flesche <chris@fletch22.com>
2018-01-29 14:55:32 -0600

    cleaning up test and adding guard for blank messages;

--- dnaStreaming/demo/show_stream.py
+++ dnaStreaming/demo/show_stream.py
@@ -5,11 +5,9 @@ listener = Listener()
 
 quiet_demo = os.getenv('QUIET_DEMO', 'false')
 
-print ('Is quiet demo true? ' + str(quiet_demo == True))
-
 def callback(message, subscription_id):
     message = message.data.__str__()
-    if quiet_demo == 'true':
+    if quiet_demo == 'true' and len(message) > 50:
         message = message[:50]
     print('Subscription ID: {}: Message: {}'.format(subscription_id, message))
     return True  # If desired return False to stop the message flow. This will unblock the process as well.
