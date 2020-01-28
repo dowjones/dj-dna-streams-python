32b06aadd2531b73b4608b53067cc29aa1c199c2
Miguel Ballesteros <miballegh@outlook.com>
2020-01-28 12:52:15 +0100

    Updated message type and display actions

--- dnaStreaming/demo/show_stream.py
+++ dnaStreaming/demo/show_stream.py
@@ -1,21 +1,25 @@
-from __future__ import absolute_import, division, print_function
-
 import os
-import json
 from dnaStreaming.listener import Listener
 
 listener = Listener()
-
 quiet_demo = os.getenv('QUIET_DEMO', "false") == "true"
+print("\n[ACTIVITY] Receiving messages (SYNC)...\n[0]", end='')
 
 
-def callback(message, subscription_id, file_handle = None):
-    if type(message) == str:
-        decoded = json.loads(message)
-        file_handle.write(json.dumps(decoded, ensure_ascii=False) + "\n")
-        #print('Subscription ID: {}: Message: {}'.format(subscription_id, message))
-        print(".")
-    print("$")
+def callback(message, subscription_id, file_handle=None):
+    callback.counter += 1
+    if not quiet_demo:
+        if message['action'] != 'del':
+            print('[INFO] [MSG] [{}]: AN: {}, TITLE: {}'.format(callback.counter, message['an'], message['title']))
+        else:
+            print('[INFO] [MSG] [{}]: AN: {}, *** DELETE ***'.format(callback.counter, message['an']))
+    else:
+        if callback.counter % 10 == 0:
+            print('[{}]'.format(callback.counter), end='')
+        else:
+            print('.', end='')
     return True
 
+
+callback.counter = 0
 listener.listen(callback)
