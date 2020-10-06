d1a681a85af217606ceee88c46b4ed773da2a9be
Miguel Ballesteros <miballegh@outlook.com>
2020-10-06 19:34:57 +0200

    Updated deps to be compliant with Oct 2020 pip req

--- setup.py
+++ setup.py
@@ -21,9 +21,8 @@ setup(
     install_requires=[
         'googleapis-common-protos>=1.6.0',
         'google-auth>=1.7.0',
-        'google-cloud-pubsub>=1.0.0',
-        'google-cloud-core>=1.0.3',
-        'google-api-core<1.17.0,>=1.14.0'
+        'google-cloud-pubsub>=1.1.0',
+        'google-cloud-core==1.3.0',
         'mock>=3.0.5',
         'oauth2client>=4.1.3',
         'requests>=2.22.0'
