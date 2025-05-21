b79961383ab5eed43d270852c2a95556fc38175f
Charlie Nino Alarcón <carlos.ninoalarcon@dowjones.com>
2025-05-21 17:23:36 -0500

    Refactor message handling in demo scripts
    - Updated message logging to use a unified format across pubsub_stream.py, show_ha_stream_async.py, show_stream_async.py, and show_stream.py.
    - Enhanced error handling for unexpected message formats and improved logging for processed messages.
    - Adjusted the duration for receiving messages in async scripts to 10 seconds for consistency.
    - Added informative print statements to track message processing progress.

--- demo/pubsub_stream.py
+++ demo/pubsub_stream.py
@@ -16,50 +16,61 @@ if gcp_project_id is not None and gcp_pubsub_topic is not None and gcp_creds is 
     listener = Listener()
     ps_publisher = pubsub_v1.PublisherClient()
     topic_path = ps_publisher.topic_path(gcp_project_id, gcp_pubsub_topic)
-    print("\n[ACTIVITY] Sending messages to Pub/Sub topic {} in GCP\n[0]".format(gcp_pubsub_topic), end='')
+    print(f"\n[ACTIVITY] Sending messages to Pub/Sub topic {gcp_pubsub_topic} in GCP...")
 
     def callback(factiva_message, subscription_id, file_handle=None):
         try:
-            callback.counter += 1
             if 'action' in factiva_message.keys():
                 # Message is an article event
                 # Implement your logic according to the documentation:
                 # https://developer.dowjones.com/documents/site-docs-factiva_apis-factiva_analytics_apis-factiva_streams_api#article-specific-events
                 if factiva_message['action'] == 'add':
                     # Insert the article to the repository as new. Handle repeated messages.
-                    logger.info(f"[ADD] AN: {factiva_message['an']} - {factiva_message['title']}")
+                    print_message(f"[ARTICLE] [ADD] AN: {factiva_message['an']} - {factiva_message['title']}")
                 elif factiva_message['action'] == 'rep':
                     # Upsert/Update/AddNewVersion the article in the repository. Handle repeated messages.
-                    logger.info(f"[REP] AN: {factiva_message['an']} - {factiva_message['title']}")
+                    print_message(f"[ARTICLE] [REP] AN: {factiva_message['an']} - {factiva_message['title']}")
                 elif factiva_message['action'] == 'del':
                     # Delete or mark as deleted the article in the repository. Handle inexistent article AN.
-                    logger.info(f"[DEL] AN: {factiva_message['an']} - *** DELETE ***")
+                    print_message(f"[ARTICLE] [DEL] AN: {factiva_message['an']} - *** DELETE ***")
                 else:
-                    logger.error(f"Factiva Action Not Handled: {factiva_message['action']}")
+                    print_message(f"Factiva Action Not Handled: {factiva_message['action']}")
+                
+                logger.info(f"Sent to GCP Pub/Sub, message with AN: {factiva_message.get('an', 'N/A')}")
+                
             elif 'event_type' in factiva_message.keys():
                 # Message is a bulk action or service event
                 if factiva_message['event_type'] == 'source_delete':
                     # Delete all articles from the repository matching the source_code. Handle inexistent source_code and repeated messages.
-                    logger.info(f"[SOURCE_DELETE] Source: {factiva_message['source_code'].upper()} - {factiva_message['description']}")
+                    print_message(f"[EVENT] [SOURCE_DELETE] Source: {factiva_message['source_code'].upper()} - {factiva_message['description']}")
                 else:
-                    logger.error(f"Factiva Event Type Not Handled: {factiva_message['event_type']}")
+                    print_message(f"Factiva Event Type Not Handled: {factiva_message['event_type']}")
+
+                logger.info(f"Sent to GCP Pub/Sub, message with event: {factiva_message.get('event_type', 'N/A')}")
+                
             else:
-                logger.error(f"Unexpected Message Format:[{factiva_message}]")
+                print_message(f"Unexpected Message Format:[{factiva_message}]")
+                
             # Publish the message to GCP Pub/Sub
             m_data = json.dumps(factiva_message, ensure_ascii=False).encode("utf-8")
             ps_publisher.publish(topic_path, data=m_data)
-            logger.info("Sent to GCP Pub/Sub, article with ID: {}".format(factiva_message.get('an', 'N/A')))
-            if callback.counter % 10 == 0:
-                print('[{}]'.format(callback.counter), end='')
-            else:
-                print('.', end='')
+            
+            callback.counter += 1
+        
+            if callback.counter % 100 == 0:
+                print_message(f"[INFO] *** Processed {callback.counter} messages ***")
+                
             return True
+        
         except Exception as e:
             logger.error(f"Error processing Factiva message: {e}")
+            # Only return False if you want to stop the listener
             return True
 
     callback.counter = 0
+    print_message(f"[INFO] *** Processed {callback.counter} messages ***")
     listener.listen(callback)
+    
 else:
     print("[ERROR]: Required ENV variables not set")
     if gcp_project_id is None:
--- demo/show_stream.py
+++ demo/show_stream.py
@@ -4,7 +4,7 @@ from dnaStreaming.listener import Listener
 
 listener = Listener()
 
-print("[ACTIVITY] Receiving messages (SYNC)...")
+print("\n[ACTIVITY] Receiving messages (SYNC)...")
 
 
 def print_message(message):
@@ -19,13 +19,13 @@ def callback(factiva_message: dict, subscription_id: str) -> bool:
             # https://developer.dowjones.com/documents/site-docs-factiva_apis-factiva_analytics_apis-factiva_streams_api#article-specific-events
             if factiva_message['action'] == 'add':
                 # Insert the article to the repository as new. Handle repeated messages.
-                print_message(f"[{factiva_message['action'].upper()}] AN: {factiva_message['an']} - {factiva_message['title']}")
+                print_message(f"[ARTICLE] [ADD] AN: {factiva_message['an']} - {factiva_message['title']}")
             elif factiva_message['action'] == 'rep':
                 # Upsert/Update/AddNewVersion the article in the repository. Handle repeated messages.
-                print_message(f"[{factiva_message['action'].upper()}] AN: {factiva_message['an']} - {factiva_message['title']}")
+                print_message(f"[ARTICLE] [REP] AN: {factiva_message['an']} - {factiva_message['title']}")
             elif factiva_message['action'] == 'del':
                 # Delete or mark as deleted the article in the repository. Handle inexistent article AN.
-                print_message(f"[{factiva_message['action'].upper()}] AN: {factiva_message['an']} - *** DELETE ***")
+                print_message(f"[ARTICLE] [DEL] AN: {factiva_message['an']} - *** DELETE ***")
             else:
                 print_message(f"[ERROR] Factiva Action Not Handled: {factiva_message['action']}")
 
@@ -33,12 +33,16 @@ def callback(factiva_message: dict, subscription_id: str) -> bool:
             # Message is a bulk action or service event
             if factiva_message['event_type'] == 'source_delete':
                 # Delete all articles from the repository matching the source_code. Handle inexistent source_code and repeated messages.
-                print_message(f"[{factiva_message['event_type'].upper()}] Source: {factiva_message['source_code'].upper()} - {factiva_message['description']}")
+                print_message(f"[EVENT] [SOURCE_DELETE] Source: {factiva_message['source_code'].upper()} - {factiva_message['description']}")
             else:
                 print_message(f"[ERROR] Factiva Event Type Not Handled: {factiva_message['event_type']}")
 
         else:
             print_message(f"[ERROR] Unexpected Message Format:[{factiva_message}]")
+            
+        callback.counter += 1
+        if callback.counter % 100 == 0:
+            print_message(f"[INFO] *** Processed {callback.counter} messages ***")
     
     except Exception as e:
         print_message(f"[ERROR] Error processing Factiva message: {e}")
@@ -49,4 +53,5 @@ def callback(factiva_message: dict, subscription_id: str) -> bool:
 
 
 callback.counter = 0
+print_message(f"[INFO] *** Processed {callback.counter} messages ***")
 listener.listen(callback)
